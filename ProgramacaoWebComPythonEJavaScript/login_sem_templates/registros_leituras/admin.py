a22b1d933aaabd23b4cae55e2ab9ed861c2e4221
walter <walteralmeidalira@gmail.com>
2024-11-21 01:19:59 -0300

    update

---
+++ ProgramacaoWebComPythonEJavaScript/login_sem_templates/registros_leituras/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from registros_leituras.models import Topic
+from registros_leituras.models import Entry
+
+# Register your models here.
+admin.site.register(Topic)
+admin.site.register(Entry)
